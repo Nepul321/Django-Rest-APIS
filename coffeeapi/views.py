bb51166fe2e56472c2d9ed97bb3dfa7715b92f9a
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-23 20:17:08 -0500

    Switch to django rest framework

--- coffeeapi/views.py
+++ coffeeapi/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def ListView(request):
-    return JsonResponse({'message' : 'Plain django'})
+    return Response({}, status=200)
